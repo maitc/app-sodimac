4d350cd8637571bb073ccbbfe2d6eb3ee7c7d0a7
maitc <maibeet.torres@gmail.com>
2017-08-29 17:30:41 -0400

    adding files

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var webserver = require('gulp-webserver');
 gulp.task('script', function(){
 	gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/bootstrap/dist/js/bootstrap.js', 'assets/js/*.js'])
 	.pipe(concat('script.js'))
-	.pipe(gulp.dest('dist/js/'));
+	.pipe(gulp.dest('/js/'));
 });
 
 gulp.task('style', function(){
@@ -16,7 +16,7 @@ gulp.task('style', function(){
 	.pipe(sass().on('error', sass.logError))
 	.pipe(minifyCSS())
 	.pipe(concat('style.min.css'))
-	.pipe(gulp.dest('dist/css/'));
+	.pipe(gulp.dest('/css/'));
 });
 
 gulp.task("webserver", function(){
